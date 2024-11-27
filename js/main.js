69d5a79af4cc0c98d4cb5467302e135d0e2b8ed2
‘notilttoday’ <tsimbaloleg2006@gmail.com>
2024-11-27 12:32:57 +0200

    PR5-3

--- js/main.js
+++ js/main.js
@@ -6,6 +6,55 @@ const closeModalWindowAuthBtn = document.querySelector('.close-auth');
 const loginButton = document.querySelector('.button-auth');
 const logoutButton = document.querySelector('.button-out');
 const userNameDisplay = document.querySelector('.user-name');
+const list = document.querySelector('.cards-restaurants')
+
+function isUserAuthorized() {
+    const savedUser = localStorage.getItem('user');
+    
+    return savedUser ? 'restaurant.html' : null;
+}
+
+function createCard() {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const link = isUserAuthorized();
+    console.log(link)
+    const cardLink = link 
+        ? `<a href="${link}" class="card card-restaurant">`
+        : `<a href="#" class="card card-restaurant" onclick="openAuthModal()">`;
+
+    card.insertAdjacentHTML('beforeend', `
+        ${cardLink}
+            <img src="img/tanuki/preview.jpg" alt="image" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title">Танукі</h3>
+                    <span class="card-tag tag">60 хвилин</span>
+                </div>
+                <!-- /.card-heading -->
+                <div class="card-info">
+                    <div class="rating">
+                        4.5
+                    </div>
+                    <div class="price">От 1 200 ₴</div>
+                    <div class="category">Суші, роли</div>
+                </div>
+                <!-- /.card-info -->
+            </div>
+            <!-- /.card-text -->
+        </a>
+    `);
+    return card;
+}
+
+function updateCards() {
+    list.innerHTML = '';
+    list.append(createCard())
+    list.append(createCard())
+    list.append(createCard())
+}
+updateCards()
 
 function openAuthModal() {
     modalWindowAuth.style.display = 'flex';
@@ -28,6 +77,7 @@ function loginUser(login) {
     userNameDisplay.textContent = login;
     userNameDisplay.style.display = 'block';
     closeAuthModal();
+    updateCards();
 }
 
 function logoutUser() {
@@ -35,6 +85,7 @@ function logoutUser() {
     loginButton.style.display = 'block';
     logoutButton.style.display = 'none';
     userNameDisplay.textContent = '';
+    updateCards();
 }
 
 window.addEventListener('load', () => {
